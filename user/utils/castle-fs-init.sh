4d15630bfc1f22418a243354ca436efed45f6d8d
Christian Sellars <csellars@acunu.com>
2010-12-22 13:50:13 +0000

    Limit the number of times a dummy page can be used to back a vmap. This is because older versions of xen (e.g. 3.0.3 on Amazon) place a restriction on this.

---
+++ user/utils/castle-fs-init.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Relies on tests script machinery to do that 
+
+set -e
+
+. /usr/share/castle-fs/init-utils
+
+if kernel_fs_running
+then
+    echo "castle-fs is already running"
+    exit 0
+fi
+
+if [ -e /var/lib/castle-fs/dirty ]
+then
+    cat <<EOF
+castle-fs was not cleanly shut down
+
+At this stage of beta, starting castle-fs on a dirty filesystem is not
+supported and will probably crash your kernel. To erase all the stored
+data and start over, please run:
+
+   acunu_nuke all
+EOF
+    exit 1
+fi
+
+disks=$(castle-scan)
+
+load_fs_and_claim $disks
+sync
+echo "All disks claimed"
+castle-cli init
+
+echo "castle-fs initialised successfully"
